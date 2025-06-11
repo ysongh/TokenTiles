c3c3eba8fc9a9a33af01fd4830f3e1eb3d0d9f0f
You Song Hou <ysongweb3@gmail.com>
2025-06-10 21:03:04 -0400

    refactor: move ConnectMenu to its own component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,7 @@
 import { sdk } from "@farcaster/frame-sdk";
 import { useEffect } from "react";
-import { useAccount, useConnect, useSignMessage } from "wagmi";
 
+import { ConnectMenu } from "./components/ConnectMenu";
 import Lobby from "./pages/Lobby";
 
 function App() {
@@ -13,52 +13,7 @@ function App() {
     <>
       <div className="bg-blue-300">Mini App + Vite + TS + React + Wagmi</div>
       <ConnectMenu />
-    </>
-  );
-}
-
-function ConnectMenu() {
-  const { isConnected, address } = useAccount();
-  const { connect, connectors } = useConnect();
-
-  if (isConnected) {
-    return (
-      <>
-        <div>Connected account:</div>
-        <div>{address}</div>
-        <SignButton />
-        <Lobby />
-      </>
-    );
-  }
-
-  return (
-    <button type="button" onClick={() => connect({ connector: connectors[0] })}>
-      Connect
-    </button>
-  );
-}
-
-function SignButton() {
-  const { signMessage, isPending, data, error } = useSignMessage();
-
-  return (
-    <>
-      <button type="button" onClick={() => signMessage({ message: "hello world" })} disabled={isPending}>
-        {isPending ? "Signing..." : "Sign message"}
-      </button>
-      {data && (
-        <>
-          <div>Signature</div>
-          <div>{data}</div>
-        </>
-      )}
-      {error && (
-        <>
-          <div>Error</div>
-          <div>{error.message}</div>
-        </>
-      )}
+      <Lobby />
     </>
   );
 }
---
+++ react/src/components/ConnectMenu.tsx
@@ -0,0 +1,46 @@
+import { useAccount, useConnect, useSignMessage } from "wagmi";
+
+export function ConnectMenu() {
+  const { isConnected, address } = useAccount();
+  const { connect, connectors } = useConnect();
+
+  if (isConnected) {
+    return (
+      <>
+        <div>Connected account:</div>
+        <div>{address}</div>
+        <SignButton />
+      </>
+    );
+  }
+
+  return (
+    <button className="bg-blue-400" type="button" onClick={() => connect({ connector: connectors[0] })}>
+      Connect
+    </button>
+  );
+}
+
+function SignButton() {
+  const { signMessage, isPending, data, error } = useSignMessage();
+
+  return (
+    <>
+      <button className="bg-blue-400" type="button" onClick={() => signMessage({ message: "hello world" })} disabled={isPending}>
+        {isPending ? "Signing..." : "Sign message"}
+      </button>
+      {data && (
+        <>
+          <div>Signature</div>
+          <div>{data}</div>
+        </>
+      )}
+      {error && (
+        <>
+          <div>Error</div>
+          <div>{error.message}</div>
+        </>
+      )}
+    </>
+  );
+}
