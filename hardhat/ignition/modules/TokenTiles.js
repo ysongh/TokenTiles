da0acaa75e59cc0f1f21035dc8dca93a4123fd55
You Song Hou <ysongweb3@gmail.com>
2025-08-15 22:09:54 -0400

    feat: add contract address for Verifiable Randomness Service

--- hardhat/ignition/modules/TokenTiles.js
+++ hardhat/ignition/modules/TokenTiles.js
@@ -3,16 +3,18 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("TokenTilesModule", (m) => {
   const token20 = m.contract("TileTokenERC20", []);
   const token1155 = m.contract("TokenTilesERC1155", []);
+  const BaseSepolia	= "	0xf4e080Db4765C856c0af43e4A8C4e31aA3b48779";
 
   const tokenTiles = m.contract("TokenTilesGame", [
     token1155,
-    token20
+    token20,
+    BaseSepolia
   ]);
 
   const setGameContract = m.call(
     token20,
     "setGameContract",
-    [tokenTiles]
+    [tokenTiles],
   );
 
   return {
