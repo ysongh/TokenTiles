26fe1747743b89ca9d74881038fe70eaf88cc55b
You Song Hou <ysongweb3@gmail.com>
2025-08-27 21:32:49 -0400

    fix: correct invalid address

--- hardhat/ignition/modules/TokenTiles.js
+++ hardhat/ignition/modules/TokenTiles.js
@@ -3,7 +3,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("TokenTilesModule", (m) => {
   const token20 = m.contract("TileTokenERC20", []);
   const token1155 = m.contract("TokenTilesERC1155", []);
-  const BaseSepolia	= "	0xf4e080Db4765C856c0af43e4A8C4e31aA3b48779";
+  const BaseSepolia	= "0xf4e080Db4765C856c0af43e4A8C4e31aA3b48779";
 
   const tokenTiles = m.contract("TokenTilesGame", [
     token1155,
